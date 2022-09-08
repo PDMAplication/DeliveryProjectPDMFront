84e3803ccd353bf8f0a51e3c33f4b9042a78bd08
Izaqui <izaquiel.silva.ads@gmail.com>
2022-09-08 20:00:36 -0300

    elementos tela Map

--- app/Pages/Map/index.tsx
+++ app/Pages/Map/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import MapView,{Marker} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
 
 export default function Map() {
 
+  const [pesquisa, setPesquisa] = useState('');
    // useState recebe objeto com todos os parametros
     let [regiao, setRegiao] = useState({
         latitude:-23.4422149,
@@ -18,21 +21,48 @@ export default function Map() {
     });
   return (
     <View style={styles.container}>
-      <MapView style={{width:'100%',height:'100%'}} initialRegion={regiao}>
+      
+      <View style={styles.inputGoogle}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        query={{
+          
+          language: 'en', // language of the results
+        }}
+        onPress={(data, details = null) => console.log(data)}
+        onFail={(error) => console.error(error)}
+        requestUrl={{
+          url:
+            'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
+          useOnPlatform: 'web',
+        }} // this in only required for use on the web. See https://git.io/JflFv more for details.
+      />
+    </View>
+      
+      <MapView style={{width:'100%',height:'80%'}} initialRegion={regiao}>
         <Marker coordinate={newCoordenada}>
-
         </Marker>
       </MapView>
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width:'100%',
+    height:'100%'
   },
-  
+  inputGoogle: {
+    flex: 1,
+    padding: 10,
+    width:'100%',
+    marginTop:'10%',
+  },
+ 
 });
